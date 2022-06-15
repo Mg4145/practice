293acb3787d06ad9f27e433ffdc351ae109036fd
mg4145 <mg4145@columbia.edu>
2022-06-15 18:27:56 -0400

    Added new thinsg learned from the Rust documentation

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -18,6 +18,10 @@ fn main() {
     /* 
      * Up next we will show  the power of shadowing in action
      * Note that Rust does not have postfix increment
+     * Shadowing is different than marking a variable as mut
+     * Using let allows us to perform transformations on a value
+     * but have the variable remain immutable
+     * You will get a compile time error if you do not use let though
      */
     
     let shadow = 5;
@@ -30,6 +34,43 @@ fn main() {
         let shadow = shadow * 2;
         println!("The value of shadow in the inner scope is {}", shadow);
     }
+
     println!("The value of shadow is {}", shadow);
     
+    /* 
+     * Using let allows us to change the type of the value but also 
+     * reuse the same name
+     */
+    
+    let spaces = "        ";
+    println!("spaces used to be of type str: {}", spaces); 
+
+    let spaces = spaces.len();
+    println!("But now spaces is of type usize with a value of {}", spaces);
+    
+    /* 
+     * Data Types
+     *  Scalar: 
+     *      1) integers
+     *      2) floating points 
+     *      3) booleans
+     *      4) characters
+     */
+    
+    /*
+     * Integer Types
+     * | Length | Signed | Unsigned |
+     * | 8-bit  |  i8    | u8       |
+     * | 16-bit |  i16   | u16      |
+     * | 32-bit |  i32   | u32      |
+     * | 64-bit |  i64   | u64      |
+     * | 128-bit|  i128  | u128     |
+     * | arch   |  isize | usize    |
+     */
+   
+    let mut value = 0;
+    while value != 10 {
+        println!("Let's get Rusty!");
+        value += 1;
+    }
 }
