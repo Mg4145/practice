ee1e1f15978932d306e096e8b41fd715a7042ad5
mg4145 <mg4145@columbia.edu>
2022-07-12 17:54:34 -0400

    Added inc insertion

--- c/insertion.c
+++ c/insertion.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+void insertionSortInc(int arr[], int n) {
+    int i, key, j;
+
+    for (i = 1; i < n; i++) {
+
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && key > arr[j]) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+
+        arr[j+1] = key;
+    }
+}
+
 void insertionSort(int arr[], int n) {
     int i, key, j;
 
@@ -19,19 +36,21 @@ void insertionSort(int arr[], int n) {
 
 void printArray(int arr[], int n) {
     int i;
-    printf("[ ");
+    printf("[");
     for(i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+        printf(" %d ", arr[i]);
     printf("]");
     printf("\n");
 }
 
 int main(int argc, char* argv[]) {
 
-    int arr[] = {5, 4, 3, 2, 1};
+    int arr[] = {5, 3, 4, 1, 2};
     int n = sizeof(arr)/sizeof(arr[0]);
     printArray(arr, n);
     insertionSort(arr, n);
+    printArray(arr, n);
+    insertionSortInc(arr, n);
     printArray(arr, n);
 
     return 0;
